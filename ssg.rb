810c7813fba37bb7913af3c15f5a8c32ab348b41
Pastor <email@sremail.pl>
2026-04-06 11:10:00 +0100

    update ssg to v1.7.10

--- ssg.rb
+++ ssg.rb
@@ -5,33 +5,36 @@
 class Ssg < Formula
   desc "Fast static site generator written in Go"
   homepage "https://github.com/spagu/ssg"
-  version "1.7.9"
+  version "1.7.10"
   license "BSD-3-Clause"
 
   on_macos do
     on_arm do
-      url "https://github.com/spagu/ssg/releases/download/v1.7.9/ssg-darwin-arm64.tar.gz"
-      sha256 "ce8f4074d3736af730d43c73394072a8bb22fdb45e8272c0447a89331d11fd1b"
+      url "https://github.com/spagu/ssg/releases/download/v1.7.10/ssg-1.7.10-darwin-arm64.tar.gz"
+      sha256 "512a0d6d898cf8f568402fab620b6cd230b5bd23d9c82cacc8cf00db9738f7ec"
     end
     on_intel do
-      url "https://github.com/spagu/ssg/releases/download/v1.7.9/ssg-darwin-amd64.tar.gz"
-      sha256 "28847986018e8806c8bf8a44dd6bb720dac2b5d5609140deb5d5622ce4f66923"
+      url "https://github.com/spagu/ssg/releases/download/v1.7.10/ssg-1.7.10-darwin-amd64.tar.gz"
+      sha256 "e7180411d7172502aedb901dafd5a1fbb4f48f9f4be62698af00a6d8507f848a"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/spagu/ssg/releases/download/v1.7.9/ssg-linux-arm64.tar.gz"
-      sha256 "b1abf0827c8da36a06c2c9d3ca207eb82b9cefd2db93d4cb6f57988afc6395d1"
+      url "https://github.com/spagu/ssg/releases/download/v1.7.10/ssg-1.7.10-linux-arm64.tar.gz"
+      sha256 "3e88a9d3ab9cc05e0f7e3dab4a56da4105f52a341cfab280113dd6f8f54b23c5"
     end
     on_intel do
-      url "https://github.com/spagu/ssg/releases/download/v1.7.9/ssg-linux-amd64.tar.gz"
-      sha256 "35e2b2c1e6562f3c2763713606730d4b4ee954c2a9914f82084dd103f216b3c0"
+      url "https://github.com/spagu/ssg/releases/download/v1.7.10/ssg-1.7.10-linux-amd64.tar.gz"
+      sha256 "0925b03ec204513359a505055751c70187ad4b1ad0bc38f3ad858868af1181b9"
     end
   end
 
   def install
-    bin.install "ssg"
+    bin.install "ssg-darwin-arm64" => "ssg" if OS.mac? && Hardware::CPU.arm?
+    bin.install "ssg-darwin-amd64" => "ssg" if OS.mac? && Hardware::CPU.intel?
+    bin.install "ssg-linux-arm64" => "ssg" if OS.linux? && Hardware::CPU.arm?
+    bin.install "ssg-linux-amd64" => "ssg" if OS.linux? && Hardware::CPU.intel?
   end
 
   test do
